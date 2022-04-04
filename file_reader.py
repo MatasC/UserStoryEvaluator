245e1c603b68ff21e3903247d8f6837271498037
MatasC <matas.cenys@mif.stud.vu.lt>
2022-04-04 11:47:58 +0300

    Updated with working evaluation model

---
+++ file_reader.py
@@ -0,0 +1,105 @@
+import os
+import spacy
+from spacy import displacy
+from pathlib import Path
+
+
+def choose_range(message, min_value, max_value, step=1):
+    while True:
+        try:
+            choice = int(input(f'{message} ({min_value}-{max_value}): '))
+        except ValueError:
+            print("Nesuprantama ivestis, bandykite dar karta.")
+            continue
+        if choice not in range(min_value, max_value + 1, step):
+            print("Neteisinga ivestis, iveskite naturaluji skaiciu is nurodyto intervalo.")
+            continue
+        else:
+            return choice
+
+
+def read_stories(path):
+    names = []
+
+    for document in os.listdir(path):
+        if document.endswith('.txt'):
+            names.append(document)
+
+    print("User story dokumentai, kuriuos galima nuskaityti:")
+    k = 0
+    for name in names:
+        print(f'{k:3} - {name}')
+        k += 1
+    choice = choose_range("Pasirinkite dokumenta nuskaitymui", 0, k - 1)
+
+    story_file = open(path + "/" + names[choice])
+
+    text = story_file.read()
+
+    return text, names[choice]
+
+
+def manual_nlp(path):
+    text, file_name = read_stories(path)
+
+    nlp = spacy.load('en_core_web_sm')
+
+    doc = nlp(text)
+
+    new_doc = []
+    sents = []
+
+    for sent in doc.sents:
+        for word in sent:
+            if word.is_stop == False and word.is_punct == False:
+                new_doc.append(word.text)
+        new_doc = nlp(" ".join(new_doc).strip())
+        sents.append(new_doc)
+        new_doc = []
+
+    sentence_spans = list(doc.sents)
+
+    options = {'distance': 150, 'compact': True}
+
+    svg = displacy.render(sents, style="dep", options=options, page=True)
+    output_path = Path(path + '/' + file_name.split('.')[0] + '.svg')
+    output_path.open("w", encoding="utf-8").write(svg)
+
+    return 0
+
+
+def prepare_story(doc):
+    words = []
+
+    multi_dependencies = []
+    for token in doc:
+        if token.dep_ != "ROOT":
+            words.append(token.dep_)
+        k = 0
+        for word in doc:
+            if str(word.head) == token.text:
+                k += 1
+
+        if k <= 1:
+            words.append(str(token.pos_))
+        else:
+            if token.dep_ == "ROOT":
+                words.append(token.pos_ + "_r")
+                continue
+            t = 1
+
+            if len(multi_dependencies) == 0:
+                words.append(str(token.pos_ + "_" + str(t)))
+                multi_dependencies.append(str(token.pos_ + "_" + str(t)))
+                continue
+
+            while True:
+                if str(token.pos_ + "_" + str(t)) not in multi_dependencies:
+                    words.append(str(token.pos_ + "_" + str(t)))
+                    multi_dependencies.append(str(token.pos_ + "_" + str(t)))
+                    break
+                else:
+                    t += 1
+
+    print(words)
+    return [word.lower() for word in words]
--- main.py
+++ main.py
@@ -1,16 +1,152 @@
-# This is a sample Python script.
+import spacy
+from spacy import displacy
+from file_reader import manual_nlp
+from file_reader import prepare_story
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import numpy
+import json
+import pickle
+from datetime import datetime
+
+import keras as keras
+
+import tflearn
+import tensorflow as tf
+
+# from tensorflow.keras.callbacks import TensorBoard
+
+nlp = spacy.load('en_core_web_sm')
+
+with open("F:/Univeras/Univeras_4_pav/Bakis/UserStoryEvaluator/data.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", 'rb') as file:
+        words, labels, training, output = pickle.load(file)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data['data']:
+        for pattern in intent['patterns']:
+            words.extend(pattern.split())
+            docs_x.append(pattern)
+            docs_y.append(intent['tag'])
+
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
+
+    words = [w.lower() for w in words]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for i in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = [0 for i in range(len(words))]
+
+        print(bag)
+
+        wrds = [w.lower() for w in doc.split()]
+
+        for w in wrds:
+            for i, word in enumerate(words):
+                if w == word:
+                    bag[i] += 1
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", 'wb') as file:
+        pickle.dump((words, labels, training, output), file)
+
+try:
+    tf.compat.v1.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+
+    model.load('model.tfl')
+
+except:
+    tf.compat.v1.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    # model = tflearn.DNN(net, tensorboard_dir='log', tensorboard_verbose=3)
+    model = tflearn.DNN(net)
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+
+    model.save('model.tfl')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def bag_of_words(story, words):
+    bag = [0 for i in range(len(words))]
+
+    nlp = spacy.load('en_core_web_sm')
+
+    story = nlp(story)
+
+    new_doc = []
+
+    for word in story:
+        if word.is_stop == False and word.is_punct == False:
+            new_doc.append(word.text)
+    story = nlp(" ".join(new_doc).strip())
+
+    story_words = prepare_story(story)
+
+    for s_word in story_words:
+        for i, word in enumerate(words):
+            if s_word == word:
+                bag[i] += 1
+    print(numpy.array(bag))
+
+    return numpy.array(bag)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_response(written_story):
+    results = model.predict([bag_of_words(written_story, words)])
+    print(results)
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+    if results[0][results_index] > 0.7:
+        for tg in data["data"]:
+            if tg['tag'] == tag:
+                return tg['responses'][0]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return "I'm sorry. Could you rephrase the question?"
+
+
+while True:
+    story = str(input("Enter a user story:"))
+    print(get_response(story))
+
+    # choice = str(input("enter 'yes' to try again - "))
+
+    # if choice == 'yes':
+    #     continue
+    # else:
+    #     break
